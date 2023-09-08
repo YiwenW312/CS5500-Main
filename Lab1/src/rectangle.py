ebc3b96137cb67e97e5f1794fd0cbf2924f6de46
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2023-09-07 20:37:32 -0700

    task 1

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -1,25 +1,43 @@
-"""
- Sample modified from CS5500, Mike Shah
+from abc import ABC, abstractmethod
 
- A rectangle class
- Note that there is no constructor or destructor,
- so a default one will be created for us.
-"""
-class Rectangle:
+class Shape(ABC):
+
+    @abstractmethod
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def set_values(self, x, y):
+        self.__width = x
+        self.__height = y
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
+    # Getter and setter for width and height if needed
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        self.__height = height
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
-
-    # Call a member function
-    rect.set_values(3, 4)
+    rect = Rectangle(3, 4)
 
     # Print out the area function
     print("area:", rect.area())
