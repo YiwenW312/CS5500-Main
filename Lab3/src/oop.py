fc40f16fe0e48e8bf919c8756b43a088b898ff33
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2023-09-28 15:07:24 -0400

    complete lab3

--- Lab3/src/oop.py
+++ Lab3/src/oop.py
@@ -1,64 +1,141 @@
+"""This module demonstrates the use of classes and objects in Python."""
 import random
 
 
 class MObject:
+    """A class to represent a generic object in a scene."""
+
     def __init__(self):
         pass
 
 
 class Image:
+    """
+    A class to represent an image.
+    """
     def __init__(self, w, h):
         print("Constructor called")
         self.m_width = w
         self.m_height = h
         self.m_colorChannels = 3  # Assume we support RGB channels only.
-        self.m_Pixels = [random.randint(0, 255) for _ in range(w * h * self.m_colorChannels)]
+        self.m_Pixels = [random.randint(0, 255)
+                         for _ in range(w * h * self.m_colorChannels)]
 
-    """
-    def __del__(self):
-        # https://docs.python.org/3/reference/datamodel.html#object.__del__
-    """
+    # def __del__(self):
+    # https://docs.python.org/3/reference/datamodel.html#object.__del__
 
     def getWidth(self):
+        """Method to get the width of the image   
+        Returns:
+            int: width of the image
+        """
         return self.m_width
 
     def getHeight(self):
+        """
+        Method to get the height of the image
+
+        Returns:
+            int: height of the image
+        """
         return self.m_height
 
-
     def getPixelColorR(self, x, y):
+        """
+        Method to get the red color of the pixel at (x, y)
+
+        Args:
+            x (_type_): _description_
+            y (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return self.m_Pixels[self.m_width * self.m_colorChannels * y + x]
 
     def getPixels(self):
+        """
+        Method to get the list of pixels
+
+        Returns:
+            _type_: _description_
+        """
         return self.m_Pixels
 
     def setPixelsToRandomValue(self):
+        """
+        Method to set the pixels to a random value
+        """
         value = random.randint(0, 255)
         # Set the entire list to one color in one function call
-        self.m_Pixels = [value] * (self.m_width * self.m_height * self.m_colorChannels)
+        self.m_Pixels = [value] * (self.m_width *
+                                   self.m_height * self.m_colorChannels)
 
 
 class Texture:
+    """a class to represent a texture in a scene
+    """
     def __init__(self, w, h):
+        """
+        Constructor
+
+        Args:
+            w 
+            h 
+        """
         self.image = Image(w, h)
 
     def getWidth(self):
+        """
+        Method to get the width of the texture
+
+        Returns:
+            int : width of the texture/image
+        """
         return self.image.getWidth()
 
     def getHeight(self):
+        """
+        Method to get the height of the texture
+
+        Returns:
+            int : height of the texture/image
+        """
         return self.image.getHeight()
-    
+
     def getPixelColorR(self, x, y):
+        """
+        Method to get the red color of the pixel at (x, y)
+
+        Args:
+            x 
+            y 
+
+        Returns:
+            int : red color of the pixel at (x, y)
+        """
         return self.image.getPixelColorR(x, y)
-    
+
     def getPixels(self):
+        """
+        Method to get the list of pixels
+
+        Returns:
+            _type_: _description_
+        """
         return self.image.getPixels()
-    
+
     def setPixelsToRandomValue(self):
+        """
+        Method to set the pixels to a random value
+        """
         self.image.setPixelsToRandomValue()
 
 
 def main():
+    """
+    Main function
+    """
     random.seed()
 
     # Create a first image
--- Lab3/src/singleton.py
+++ Lab3/src/singleton.py
@@ -1,28 +1,58 @@
+""" This module implements the Logger class as a singleton class."""
+
+
 class Logger:
+    """
+    A singleton class that allows adding messages to a list of messages.
+
+    This class ensures that only one instance of the Logger class is created
+    throughout the lifetime of the program. The instance can be accessed using
+    the `instance` class method.
+
+    Attributes:
+        messages (list): A list of messages added to the logger.
+    """
+
     _instance = None  # Private class variable to hold the single instance
 
-    def __private_init(self): 
+    def __init__(self):
+        """
+        Private method to initialize the instance of the Logger class.
+        This method is called only once when the instance is created.
+        """
         self.messages = []
 
     @classmethod
-    def instance(cls):  # Class method to get the single instance
+    def instance(cls):
+        """Class method to get the single instance
+        Attributes:
+            cls
+        """
         if cls._instance is None:
             cls._instance = cls()  # Creating an instance
-            cls._instance.__private_init()  # Initializing the instance
+            # cls._instance.__init__()  # Initializing the instance
             print("Logger created exactly once")
         else:
             print("logger already created")
         return cls._instance
 
     def add_message(self, message):
+        """Method to add a message to the list of messages
+
+        Args:
+            message
+        """
         self.messages.append(message)
 
 
 def main():
+    """
+    Main function to test the Logger class
+    """
     # Logger should only be initialized one time if it is properly
     # refactored as a singleton class
     for i in range(3):
-        logger = Logger()
+        logger = Logger().instance()
         logger.add_message(f"Adding message number: {i}")
 
 
